91457b5d2b78b583e2a2b59356321b78e36e90e6
Enikov <enigmaenikov@gmail.com>
2018-10-20 16:20:36 +0300

    Implemented EfGenericRepository with interface needed for the application.

---
+++ CustomerManager/CustomerManager.Data/Repositories/EfGenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManager.Data.Repositories
+{
+    public class EfGenericRepository<T> : IEfGenericRepository<T> where T : class
+    {
+        public EfGenericRepository(INorthwindDbContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentException("An instance of DbContext is required to use this repository.", "context");
+            }
+
+            this.Context = context;
+            this.DbSet = this.Context.Set<T>();
+        }
+
+        protected IDbSet<T> DbSet { get; set; }
+
+        protected INorthwindDbContext Context { get; set; }
+
+        public virtual IEnumerable<T> All()
+        {
+            return this.DbSet.AsEnumerable();
+        }
+
+        public virtual T GetById(object id)
+        {
+            return this.DbSet.Find(id);
+        }
+    }
+}
+
---
+++ CustomerManager/CustomerManager.Data/Repositories/IEfGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManager.Data.Repositories
+{
+    public interface IEfGenericRepository<T>
+          where T : class
+    {
+        IEnumerable<T> All();
+
+        T GetById(object id);
+    }
+}
