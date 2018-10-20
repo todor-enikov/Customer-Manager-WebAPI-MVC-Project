3a1c252b14626fbd04006546defed1b4b4bf8e78
Enikov <enigmaenikov@gmail.com>
2018-10-20 16:32:19 +0300

    Implemented current Service layer for the application.

---
+++ CustomerManager/CustomerManager.Services/Contracts/ICustomerService.cs
@@ -0,0 +1,18 @@
+﻿using CustomerManager.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManager.Services.Contracts
+{
+    public interface ICustomerService
+    {
+        IEnumerable<Customer> GetAll();
+
+        Customer GetById(object id);
+
+        IEnumerable<Order> GetOrdersByCustomerId(object id);
+    }
+}
---
+++ CustomerManager/CustomerManager.Services/CustomerService.cs
@@ -0,0 +1,42 @@
+﻿using CustomerManager.Data.Models;
+using CustomerManager.Data.Repositories;
+using CustomerManager.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManager.Services
+{
+    public class CustomerService : ICustomerService
+    {
+
+        private readonly IEfGenericRepository<Customer> customerRepository;
+
+        public CustomerService(IEfGenericRepository<Customer> repository)
+        {
+            if (repository == null)
+            {
+                throw new ArgumentException("The customer repository should not be null!");
+            }
+
+            this.customerRepository = repository;
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            return this.customerRepository.All();
+        }
+
+        public Customer GetById(object id)
+        {
+            return this.customerRepository.GetById(id);
+        }
+
+        public IEnumerable<Order> GetOrdersByCustomerId(object id)
+        {
+            return this.customerRepository.GetById(id).Orders;
+        }
+    }
+}
