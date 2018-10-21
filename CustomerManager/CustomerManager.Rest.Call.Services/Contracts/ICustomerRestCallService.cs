ab3c66d277b8c2211e1c251d0c8191b3829a3e19
Enikov <enigmaenikov@gmail.com>
2018-10-21 21:11:05 +0300

    Added CustomerManager.Rest.Call.Services project, needed for the rest calls to the WebAPI and implemented the GetAllCustomers method.

---
+++ CustomerManager/CustomerManager.Rest.Call.Services/Contracts/ICustomerRestCallService.cs
@@ -0,0 +1,14 @@
+﻿using CustomerManager.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManager.Rest.Call.Services.Contracts
+{
+    public interface ICustomerRestCallService
+    {
+        IEnumerable<CustomerModel> GetAllCustomers();
+    }
+}
---
+++ CustomerManager/CustomerManager.Rest.Call.Services/CustomerRestCallService.cs
@@ -0,0 +1,31 @@
+﻿using CustomerManager.Common.Models;
+using CustomerManager.Rest.Call.Services.Contracts;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManager.Rest.Call.Services
+{
+    public class CustomerRestCallService : ICustomerRestCallService
+    {
+        private const string baseURL = "http://localhost:56638";
+        private readonly IRestClient client;
+
+        public CustomerRestCallService(IRestClient client)
+        {
+            this.client = new RestClient(baseURL);
+        }
+
+        public IEnumerable<CustomerModel> GetAllCustomers()
+        {
+            var getAllCustomersRequest = new RestRequest("api/customers");
+
+            var response = this.client.Execute<List<CustomerModel>>(getAllCustomersRequest).Data;
+
+            return response;
+        }
+    }
+}
