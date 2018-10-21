95db62debdd4743269256ed1a7509cd528718ce3
Enikov <enigmaenikov@gmail.com>
2018-10-21 20:53:27 +0300

    Added project for common models.

---
+++ CustomerManager/CustomerManager.Common.Models/CustomerModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManager.Common.Models
+{
+    public class CustomerModel
+    {
+        public string ContractName { get; set; }
+
+        public int OrdersCount { get; set; }
+    }
+}
