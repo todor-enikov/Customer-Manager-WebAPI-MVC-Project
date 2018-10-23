9424ff91e1bebe8beaddb24c44166c7709980beb
Enikov <enigmaenikov@gmail.com>
2018-10-23 20:29:13 +0300

    Created ErrorController and view for the controller.

---
+++ CustomerManager/CustomerManager.MVC.Client/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomerManager.MVC.Client.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
